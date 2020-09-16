393909ac34556eccd8372407526fb7c75c3e8018
Kristoffer Ellersgaard Koch <kristoffer@huddly.com>
2020-09-16 10:04:15 +0200

    Factor uv_mutex into wrapper class

--- cpp/queue_uv.cpp
+++ cpp/queue_uv.cpp
@@ -1,5 +1,8 @@
 #include "queue_uv.hpp"
+#include "mutex_uv.hpp"
+
 #include <uv.h>
+
 #include <queue>
 #include <cassert>
 
@@ -7,50 +10,46 @@ struct QueueUv : public Queue {
     QueueUv(std::function<void(void)> notify)
         : notify(std::move(notify))
         , cond{}
-        , mutex{}
+        , mutex()
         , queue()
     {
-        int r = uv_mutex_init(&mutex);
-        assert(r == 0);
-        r = uv_cond_init(&cond);
+        int const r = uv_cond_init(&cond);
         assert(r == 0);
     }
     void push(QueueItemPtr item) override {
-        uv_mutex_lock(&mutex);
-        queue.push(std::move(item));
-        uv_cond_signal(&cond);
-        uv_mutex_unlock(&mutex);
+        {
+            auto lock = mutex.lock();
+            queue.push(std::move(item));
+            uv_cond_signal(&cond);
+        }
         if (notify) {
             notify();
         }
     }
 
     QueueItemPtr pop() override {
-        uv_mutex_lock(&mutex);
+        auto lock = mutex.lock();
         while (queue.empty()) {
-            uv_cond_wait(&cond, &mutex);
+            uv_cond_wait(&cond, &mutex.mutex);
         }
         auto item = std::move(queue.front());
         queue.pop();
-        uv_mutex_unlock(&mutex);
         return item;
     }
 
     QueueItemPtr pop_nowait() override {
-        uv_mutex_lock(&mutex);
+        auto lock = mutex.lock();
         if (queue.empty()) {
-            uv_mutex_unlock(&mutex);
             return nullptr;
         }
         auto item = std::move(queue.front());
         queue.pop();
-        uv_mutex_unlock(&mutex);
         return item;
     }
 private:
     std::function<void(void)> const notify;
     uv_cond_t cond;
-    uv_mutex_t mutex;
+    MutexUv mutex;
     std::queue<QueueItemPtr> queue;
 };
 
