9b66e5c466766a69ce8d01dd4f2d666274dcfe60
Brikend <brikend@huddly.com>
2022-03-17 09:03:44 +0100

    feat: Remove all the cpp implementation & setup

--- cpp/queue_uv.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "queue_if.hpp"
-#include <functional>
-
-QueuePtr make_uv_queue(std::function<void(void)> notify);
-static inline QueuePtr make_uv_queue() {return make_uv_queue(nullptr); }
